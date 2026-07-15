6625e5cbd8f72f9d09b2f293b6ec8d60900a82d9
HthePaul2 <160049330+HthePaul2@users.noreply.github.com>
2026-07-15 19:43:42 +0300

    Register review verification migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,6 +1,7 @@
-import * as migration_20260715_140848_initial from './20260715_140848_initial';
-import * as migration_20260715_144136 from './20260715_144136';
-import * as migration_20260715_144639 from './20260715_144639';
+import * as migration_20260715_140848_initial from './20260715_140848_initial'
+import * as migration_20260715_144136 from './20260715_144136'
+import * as migration_20260715_144639 from './20260715_144639'
+import * as migration_20260715_194500_review_verification from './20260715_194500_review_verification'
 
 export const migrations = [
   {
@@ -16,6 +17,11 @@ export const migrations = [
   {
     up: migration_20260715_144639.up,
     down: migration_20260715_144639.down,
-    name: '20260715_144639'
+    name: '20260715_144639',
   },
-];
+  {
+    up: migration_20260715_194500_review_verification.up,
+    down: migration_20260715_194500_review_verification.down,
+    name: '20260715_194500_review_verification',
+  },
+]
